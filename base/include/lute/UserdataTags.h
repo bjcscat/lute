8c8ebf01e1c4c750057c905c73577b4c71097605
bjcscat <bjcs.rbx@gmail.com>
2025-05-09 09:16:27 -0500

    move userdata tags headerfile to base

--- base/include/lute/UserdataTags.h
+++ base/include/lute/UserdataTags.h
@@ -1,7 +1,11 @@
 #pragma once
 
 namespace userdata_tags {
+    // runtime
     constexpr int REQUIRE_CONTEXT = 1;
-
+    // fs
     constexpr int FILE_HANDLE = 2;
+    // time
+    constexpr int DURATION_TAG = 3;
+    constexpr int INSTANT_TAG = 4;
 }
